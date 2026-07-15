e929779165e053f5532a4d0a40dcb410fbbcd07d
vedikaagarwal1277 <vedikaagarwal1277@gmail.com>
2026-07-15 12:18:48 +0530

    arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -1,28 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    int n,x,i,cnt=0;
+    int n,x,i,count=0;
 
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+    printf("Enter number of elements:");
+    scanf("%d",&n);
 
     int arr[n];
 
     printf("Enter the array elements:\n");
-    for(i = 0; i < n; i++) {
+    for(i = 0;i<n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    printf("Enter the number to search: ");
-    scanf("%d", &x);
+    printf("Enter the number to search:");
+    scanf("%d",&x);
 
-    for(i = 0; i < n; i++) {
-        if(arr[i] == x) {
-            cnt++;
+    for(i=0;i<n;i++) {
+        if(arr[i]==x) {
+            count++;
         }
     }
 
-    printf("%d occurs %d times.\n", x, cnt);
+    if(count>0) {
+        printf("%d exists and occurs %d times.\n",x,count);
+    } else {
+        printf("%d does not exist.\n",x);
+    }
 
     return 0;
 }
